0dfa9380378f5d3e9d97eca18e9e9785962e5aa1
nickhealthy <alskadmlcraz1@gmail.com>
2025-07-14 17:49:59 +0900

    build: src/main/resources/application.yml

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -4,16 +4,17 @@ import com.example.querydsl.entity.Hello;
 import com.example.querydsl.entity.QHello;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
