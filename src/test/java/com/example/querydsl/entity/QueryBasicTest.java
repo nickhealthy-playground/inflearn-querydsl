cb642c1e16a99fe4954b4290cc78b33622f40087
nickhealthy <alskadmlcraz1@gmail.com>
2025-07-17 18:53:03 +0900

    3. 기본 문법 - 정렬

--- src/test/java/com/example/querydsl/entity/QueryBasicTest.java
+++ src/test/java/com/example/querydsl/entity/QueryBasicTest.java
@@ -148,4 +148,32 @@ public class QueryBasicTest {
         assertThat(totalCount).isEqualTo(4);
 
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 오름차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    void sort() {
+        queryFactory = new JPAQueryFactory(em);
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.name.asc().nullsLast()) // nullsLast(), nullsFirst(): null 데이터 순서 부여
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getName()).isEqualTo("member5");
+        assertThat(member6.getName()).isEqualTo("member6");
+        assertThat(memberNull.getName()).isNull();
+    }
 }
