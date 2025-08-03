3165c28bab8b43e5090f9d0d81726e23dccfe357
nickhealthy <alskadmlcraz1@gmail.com>
2025-08-03 16:46:51 +0900

    6.스프링 데이터 JPA와 QueryDSL - 스프링 데이터 페이징 활용3(컨트롤러 개발)

--- src/main/java/com/example/querydsl/controller/MemberController.java
+++ src/main/java/com/example/querydsl/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.example.querydsl.controller;
 import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.repository.MemberJpaRepository;
+import com.example.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,20 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> members(MemberSearchCondition condition) {
         return memberJpaRepository.searchByWhere(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMembers2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMembers3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchComplex(condition, pageable);
     }
 }
